918b3dcaae59e0a1f744064465836d68f62e90dd
gravestench <dknuth0101@gmail.com>
2021-06-01 10:17:21 -0700

    minor changes to dcc-check

--- cmd/dcc-check/main.go
+++ cmd/dcc-check/main.go
@@ -2,6 +2,7 @@ package dcc_check
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 )
@@ -13,13 +14,25 @@ import (
 func main() {
 	srcPath := flag.Arg(1)
 
+	if srcPath == "" {
+		printUsage()
+		return
+	}
+
 	fileContents, err := ioutil.ReadFile(srcPath)
 	if err != nil {
-		log.Fatal(err)
+		const fmtErr = "could not read file, %v"
+		log.Fatal(fmt.Errorf(fmtErr, err))
 	}
 
 	_, err = dcc.FromBytes(fileContents)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func printUsage() {
+	str := fmt.Sprintf("Usage:\r\n\t%v path/to/file.dcc", flag.Arg(0))
+
+	log.Print(str)
 }
